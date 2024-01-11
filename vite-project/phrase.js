59c734ff9827d510890ee21f99379ce24df61906
AnthonyPvZ <anthonypvz123@gmail.com>
2024-01-11 09:23:27 -0500

    k

---
+++ vite-project/phrase.js
@@ -0,0 +1,92 @@
+export {phrases}
+const phrases = [{
+    Line: "Rock on!",
+  },
+  {
+    Line: "Rock and Stone... Yeeaaahhh!",
+  },
+  {
+    Line: "Rock and Stone forever!",
+  },
+  {
+    Line: "ROCK... AND... STONE!",
+  },
+  {
+    Line: "Rock and Stone!",
+  },
+  {
+    Line: "For Rock and Stone!",
+  },
+  {
+    Line: "We are unbreakable!",
+  },
+  {
+    Line: "Rock and roll!",
+  },
+  {
+    Line: "Rock and roll and stone",
+  },
+  {
+    Line: "That's it lads! Rock and stone!",
+  },
+  {
+    Line: "Like that! Rock and Stone!",
+  },
+  {
+    Line: "Yeaahhh! Rock and Stone!",
+  },
+  {
+    Line: "None can stand before us!",
+  },
+  {
+    Line: "Rock solid!",
+  },
+  {
+    Line: "Stone and Rock! ...Oh, wait...",
+  },
+  {
+    Line: "Come on guys! Rock and Stone!",
+  },
+  {
+    Line: "If you don't Rock and Stone, you ain't comin' home!",
+  },
+  {
+    Line: "We fight for Rock and Stone!",
+  },
+  {
+    Line: "We rock!",
+  },
+  {
+    Line: "Rock and Stone everyone!",
+  },
+  {
+    Line: "Stone.",
+  },
+  {
+    Line: "Yeah, yeah, Rock and Stone",
+  },
+  {
+    Line: "Rock and Stone in the Heart!",
+  },
+  {
+    Line: "For Teamwork!",
+  },
+  {
+    Line: "Did I hear a Rock and Stone?",
+  },
+  {
+    Line: "Rock and Stone, Brother!",
+  },
+  {
+    Line: "Rock and Stone to the Bone!",
+  },
+  {
+    Line: "For Karl!",
+  },
+  {
+    Line: "Leave No Dwarf Behind!",
+  },
+  {
+    Line: "By the Beard!",
+  },
+]
