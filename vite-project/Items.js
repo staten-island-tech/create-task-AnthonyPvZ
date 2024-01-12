dd636d8d099f1919a6c6195976664365e5ccda12
AnthonyPvZ <anthonypvz123@gmail.com>
2024-01-12 09:23:26 -0500

    Structure

---
+++ vite-project/Items.js
@@ -0,0 +1,63 @@
+export {Melees, guns, other, traits}
+const Melees = [{
+    Name: Crowbar,
+    Worth: 30,
+  },
+  {
+    Name: Axe,
+    Worth: 30,
+  },
+  {
+    Name: Stun_Baton, 
+    Worth: 30,
+  },
+  {
+    Name: Knife,
+    Worth: 15,
+  },
+  {
+    Namme: Brass_Knuckles,
+    Worth: 15,
+  },
+]
+const tools = [{
+  Name: Medkit,
+  Worth: 45,
+},
+{
+  Name: Medic_Dart_Pistol,
+  Worth: 45,
+}
+]
+const guns = [{
+  Name: Famas,
+  Worth: 45,
+},
+{
+  Name: Shotgun,
+  Worth: 45,
+},
+{
+  Name: Pistol,
+  Worth: 45,
+}]
+
+const other = [{
+  Name: C-Foam_Grenade,
+  Worth: 20,
+},
+{
+  Name: Palisade_Barricade_Kit,
+  Worth: 20,
+}]
+
+const traits = [{
+  Name: Athletic,
+  Description: "+10hp, Lets you sprint for +33 speed while consuming stamina",
+  Worth: 20,
+},
+{
+  Name: Firearms_Training,
+  Description: "+5% firerate, 10% accuracy, +5% reload speed",
+  Worth: 20,
+}]
--- vite-project/main.js
+++ vite-project/main.js
@@ -1,2 +1,15 @@
 import './style.css';
-import {phrases} from './phrase.js';
+import {Melees, guns, other, traits} from './Items';
+
+function Insert(blah) {
+    blah.map((result) => {
+      const card = document.createElement('div');
+      card.classlist = 'card';
+      const description = `<div class="card"> 
+      <h2 class="card-title">${result.title}</h2>
+      <img src="${result.image}" alt="" class="card-img">
+      <p class="card-desc">${result.description}</p>
+    </div>`;
+      container.innerHTML += description;
+    })
+  }
