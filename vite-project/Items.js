ab3abe9eb154518fdf8046beea9f622ea9d40bd0
AnthonyPvZ <anthonypvz123@gmail.com>
2024-01-16 09:22:56 -0500

    filtering

--- vite-project/Items.js
+++ vite-project/Items.js
@@ -1,63 +1,73 @@
-export {Melees, guns, other, traits}
-const Melees = [{
-    Name: Crowbar,
-    Worth: 30,
+export {Store}
+const Store = [{
+    Name: "Crowbar",
+    Worth: "30",
+    type: "Melee Weapons",
   },
   {
-    Name: Axe,
-    Worth: 30,
+    Name: "Axe",
+    Worth: "30",
+    type: "Melee Weapons",
   },
   {
-    Name: Stun_Baton, 
-    Worth: 30,
+    Name: "Stun Baton", 
+    Worth: "30",
+    type: "Melee Weapons",
   },
   {
-    Name: Knife,
-    Worth: 15,
+    Name: "Knife",
+    Worth: "15",
+    type: "Melee Weapons",
   },
   {
-    Namme: Brass_Knuckles,
-    Worth: 15,
+    Name: "Brass Knuckles",
+    Worth: "15",
+    type: "Melee Weapons",
   },
-]
-const tools = [{
-  Name: Medkit,
-  Worth: 45,
-},
-{
-  Name: Medic_Dart_Pistol,
-  Worth: 45,
-}
-]
-const guns = [{
-  Name: Famas,
-  Worth: 45,
-},
-{
-  Name: Shotgun,
-  Worth: 45,
-},
-{
-  Name: Pistol,
-  Worth: 45,
-}]
-
-const other = [{
-  Name: C-Foam_Grenade,
-  Worth: 20,
-},
-{
-  Name: Palisade_Barricade_Kit,
-  Worth: 20,
-}]
-
-const traits = [{
-  Name: Athletic,
-  Description: "+10hp, Lets you sprint for +33 speed while consuming stamina",
-  Worth: 20,
-},
-{
-  Name: Firearms_Training,
-  Description: "+5% firerate, 10% accuracy, +5% reload speed",
-  Worth: 20,
-}]
+  {
+  Name: "Medkit",
+  Worth: "45",
+  type: "Tools",
+  },
+  {
+    Name: "Medic Dart Pistol",
+    Worth: "45",
+    type: "Tools",
+  },
+  {
+    Name: "Famas",
+    Worth: "45",
+    type: "Guns",
+  },
+  {
+    Name: "Shotgun",
+    Worth: "45",
+    type: "Guns",
+  },
+  {
+    Name: "Pistol",
+    Worth: "45",
+    type: "Guns",
+  },
+  {
+    Name: "C-Foam Grenade",
+    Worth: "20",
+    type: "Other",
+  },
+  {
+    Name: "Palisade Barricade Kit",
+    Worth: "20",
+    type: "Other",
+  },
+  {
+    Name: "Athletic",
+    Description: "+10hp, Lets you sprint for +33 speed while consuming stamina",
+    Worth: "20",
+    type: "Traits",
+  },
+  {
+    Name: "Firearms Training",
+    Description: "+5% firerate, 10% accuracy, +5% reload speed",
+    Worth: "20",
+    type: "Traits",
+  }]
--- vite-project/main.js
+++ vite-project/main.js
@@ -1,15 +1,23 @@
 import './style.css';
-import {Melees, guns, other, traits} from './Items';
+import {Store} from './Items';
 
 function Insert(blah) {
     blah.map((result) => {
       const card = document.createElement('div');
       card.classlist = 'card';
       const description = `<div class="card"> 
-      <h2 class="card-title">${result.title}</h2>
+      <h2 class="card-title">${result.Name}</h2>
       <img src="${result.image}" alt="" class="card-img">
-      <p class="card-desc">${result.description}</p>
+      <p class="card-desc">${result.Worth}</p>
     </div>`;
       container.innerHTML += description;
     })
   }
+  let buttonse = document.querySelectorAll(".button")
+  buttonse.forEach((btn) => btn.addEventListener("click", function () {
+    let filter = btn.textContent
+    container.innerHTML = ""
+    let newarr = Store.filter((option) => { return option.type === filter })
+    if (filter === "Reset"){Insert(cards)}
+    else {Insert(newarr)}
+  }))
