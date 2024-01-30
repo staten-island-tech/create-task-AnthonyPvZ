d65831b9cda71d3a785e68ef9b68450c26b98514
AnthonyPvZ <anthonypvz123@gmail.com>
2024-01-30 09:20:13 -0500

    d

--- vite-project/main.js
+++ vite-project/main.js
@@ -10,12 +10,13 @@ function Insert(blah) {
       <h2 class="card-title">${result.Name}</h2>
       <img src="${result.image}" alt="" class="card-img">
       <p class="card-desc">${result.Worth}</p>
-      <button type="button" class=buy>Click to add ${result.Name} to cart</button>
+      <button type="button" class=buy>${result.Name}</button>
       </div>`;
       container.innerHTML += description;
     })
   }
-  let filters = document.querySelectorAll(".button")
+
+let filters = document.querySelectorAll(".button")
   filters.forEach((btn) => btn.addEventListener("click", function () {
     let filter = btn.textContent
     container.innerHTML = ""
@@ -23,15 +24,17 @@ function Insert(blah) {
     if (filter === "Reset"){Insert(cards)}
     else {Insert(newarr)}
   }))
-  let buying = document.querySelectorAll(".buy")
-  buying.forEach((btn) => btn.addEventListener("click"), function() {
-    let search = btn.textContent
-    let newarr = Store.filter((option) => { return option.type = search })
-    if (search = "Reset"){Insert(cards)}
-    else {Purchase(newarr)}
-  })
 
-  function Purchase(blah) {
+let buying = document.querySelectorAll(".buy")
+  buying.forEach((btn) => btn.addEventListener("click", function() {
+    console.log("pressed")
+    let search = btn.textContent
+    let product = Store.map((option) => { return option.Name === search })
+    if (search === "Reset"){Insert(cards)}
+    else {Insert(product)}
+  }))
+
+function Purchase(blah) {
     blah.map((result) => {
       const card = document.createElement('div');
       card.classlist = 'card';
@@ -44,3 +47,12 @@ function Insert(blah) {
       checkout.innerHTML += description;
     })
   }
+
+function Checkout(cart){
+    cart.map((result)=>{
+
+    })
+  }
+  // to do - fix add functionality -- try removeparent element
+  // purchase function will add array items into a new array called cart 
+  // check out function will add the prices of all items and compare it to the budget, returning a yes or no response
